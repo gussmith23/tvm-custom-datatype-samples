801f0256754a00c9741f556faa6022484a559592
Gus Smith <gussmith@cs.washington.edu>
2018-11-27 14:40:15 -0800

    Rename and simplify

--- add.py
+++ add.py
@@ -1,10 +1,9 @@
 import tvm
 
 tvm.register_datatype("foo", 23)
-tvm.register_datatype("bar", 23)
 
 x = tvm.placeholder((3,), name="x", dtype="custom[foo]32")
-y = tvm.placeholder(x.shape, name="y", dtype="custom[bar]32")
+y = tvm.placeholder(x.shape, name="y", dtype="custom[foo]32")
 z = tvm.compute(x.shape, lambda i: x[i] + y[i])
 
 s = tvm.create_schedule([z.op])
