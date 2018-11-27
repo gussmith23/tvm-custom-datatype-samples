935d35b567817605ebbd76cf540f99ed9f54844c
Gus Smith <gussmith@cs.washington.edu>
2018-11-27 14:30:00 -0800

    Update to simple working example

--- tvm-example.py
+++ tvm-example.py
@@ -5,18 +5,12 @@ import nnvm.testing
 import tvm
 from tvm.contrib import graph_runtime
 
-x = tvm.placeholder((3,), name="x", dtype="custom[mycustomtype]32")
-y = tvm.placeholder(x.shape, name="y", dtype="custom[mycustomtypetwo]32")
+tvm.register_datatype("foo", 23)
+tvm.register_datatype("bar", 23)
+
+x = tvm.placeholder((3,), name="x", dtype="custom[foo]32")
+y = tvm.placeholder(x.shape, name="y", dtype="custom[bar]32")
 z = tvm.compute(x.shape, lambda i: x[i] + y[i])
 
-print(z.debug_str())
-
-# batch_size = 1
-# num_class = 1000
-# image_shape = (3, 224, 224)
-# data_shape = (batch_size,) + image_shape
-# out_shape = (batch_size, num_class)
-# 
-# net, params = nnvm.testing.resnet.get_workload(
-#     layers=18, batch_size=batch_size, image_shape=image_shape)
-# print(net.debug_str())
+s = tvm.create_schedule([z.op])
+print(tvm.lower(s, [x, y, z], simple_mode=True))
